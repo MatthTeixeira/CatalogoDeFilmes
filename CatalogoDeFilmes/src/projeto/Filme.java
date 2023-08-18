6426dc53ad37767d966478b622f19e868aff853a
mathvelozzo <mathvelozzo@gmail.com>
2023-08-18 18:28:17 -0300

    Estruturação da classe filme

--- CatalogoDeFilmes/src/projeto/Filme.java
+++ CatalogoDeFilmes/src/projeto/Filme.java
@@ -1,4 +1,28 @@
 package projeto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Filme {
+    private String nome;
+    private String dataLancamento;
+    private String orcamento;
+    private String descricao;
+    private Diretor diretor;
+    private List<Ator> atores;
+
+    public Filme(String nome, String dataLancamento, String orcamento, String descricao, Diretor diretor) {
+        this.nome = nome;
+        this.dataLancamento = dataLancamento;
+        this.orcamento = orcamento;
+        this.descricao = descricao;
+        this.diretor = diretor;
+        this.atores = new ArrayList<>();
+    }
+
+    public Filme() {
+
+        atores = new ArrayList<Ator>();
+
+    }
 }
